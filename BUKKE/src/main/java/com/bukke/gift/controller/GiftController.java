90f94659fbe3f7ed168ee963c57bce7a03ac23e5
CHANHEE0621 <cksgml0621@naver.com>
2021-05-17 22:40:56 +0900

    GiftController 수정 - 찬희

--- BUKKE/src/main/java/com/bukke/gift/controller/GiftController.java
+++ BUKKE/src/main/java/com/bukke/gift/controller/GiftController.java
@@ -1,5 +1,45 @@
 package com.bukke.gift.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bukke.gift.service.GiftService;
+
+@Controller
 public class GiftController {
+	
+	@Autowired
+	private GiftService gService;
+	
+	// 선물하기 전체 보기
+	@RequestMapping(value="")
+	public String GiftListView() {
+		return null;
+	}
+	
+	// 선물하기 상세 보기
+	@RequestMapping(value="")
+	public String GiftDetailView() {
+		return null;
+	}
+	
+	// 선물하기 (등록) jsp 이동 (뷰)
+	@RequestMapping(value="")
+	public String sendGiftView() {
+		return null;
+	}
+	
+	// 선물하기 (등록) 기능 메소드
+	@RequestMapping(value="")
+	public String sendGift() {
+		return null;
+	}
+	
+	// 선물하기 취소(삭제)
+	@RequestMapping(value="")
+	public String GiftRemove() {
+		return null;
+	}
 
 }

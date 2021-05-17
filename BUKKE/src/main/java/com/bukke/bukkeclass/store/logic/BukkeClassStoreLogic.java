286138aff3a12893ca817be7ad10815169bd7c4a
CHANHEE0621 <cksgml0621@naver.com>
2021-05-17 17:14:16 +0900

    홍찬희 class 수정

--- BUKKE/src/main/java/com/bukke/bukkeclass/store/logic/BukkeClassStoreLogic.java
+++ BUKKE/src/main/java/com/bukke/bukkeclass/store/logic/BukkeClassStoreLogic.java
@@ -1,7 +1,53 @@
 package com.bukke.bukkeclass.store.logic;
 
+import java.util.ArrayList;
+
+import com.bukke.bukkeclass.domain.BukkeClass;
+import com.bukke.bukkeclass.domain.BukkeClassPageInfo;
 import com.bukke.bukkeclass.store.BukkeClassStore;
 
 public class BukkeClassStoreLogic implements BukkeClassStore {
+
+	@Override
+	public ArrayList<BukkeClass> selectAllList(BukkeClassPageInfo pi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<BukkeClass> selectAllList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BukkeClass selectOne(int classNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<BukkeClass> selectSearchList(String searchValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertBukkeClass(BukkeClass bClass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateBukkeClass(BukkeClass bClass) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBukkeClass(int classNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
